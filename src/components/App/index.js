04a3cce511b279ee91fe7c4a65d37969c8b6c4e6
Jose Maria Ocaña <Jose Maria Ocaa>
2017-05-08 12:02:00 +0200

    añadida libreria prop-types y actualizado componente boton con uso de props

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,40 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
-import styled, { ThemeProvider } from 'styled-components';
+import styled from 'styled-components';
 import { HashRouter, Match } from 'react-router';
 
+//organism
 import Header from '../core/organisms/Header'; 
 import Main from '../core/organisms/Main'; 
 import Footer from '../core/organisms/Footer'; 
 
-// Set the theme to royalblue in this parent
-const BlueSection = ({children}) => (
-  <ThemeProvider theme={{
-    main: 'royalblue'
-  }}>
-    { children }
-  </ThemeProvider>
-)
+//atoms
+import Button from '../core/atoms/Button'; 
+import Date from '../core/atoms/Date'; 
+import Logo from '../core/atoms/Logo'; 
 
-const App = () =>
-    <HashRouter>
-        <div>
-            <Header />
-            <Match exactly pattern='/' render={() => {
-                return(
-                    <p>Pantalla tweets</p>
-                    )
-                }
-            } />
-
-            <Match  pattern='/profile' render={() => {
-                return(
-                    <p>Pantalla perfil</p>
-                    )
-                }
-            }  />
-            <Footer />
-        </div>
-    </HashRouter>    
-
+const App = () => <div>
+    <Button />
+    <Button small/>
+</div>                
+                
 export default App;
